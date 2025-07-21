a3edb68405f7003955663ee13f9fece223b5d729
Kimheang-code-IT <heang015873174@gmail.com>
2025-07-21 21:20:09 +0700

    version 0.0.0.1 structure project

---
+++ test_connection.py
@@ -0,0 +1,11 @@
+# test_connection.py
+from db_connection import get_connection
+
+def test():
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT 'Hello, Oracle!' FROM dual")
+    print(cur.fetchone()[0])
+
+if __name__ == "__main__":
+    test()
